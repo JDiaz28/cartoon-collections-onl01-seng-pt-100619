79b81bf52841c52b032b11b5cbb7a0d9a587d138
Abraham Moreno <adiaz2821@gmail.com>
2019-11-19 14:55:23 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,9 +12,9 @@ def summon_captain_planet(veggies)  # code an argument here
   end 
 end
 
-def long_planeteer_calls(word) # code an argument here
+def long_planeteer_calls(calls_long) # code an argument here
   # Your code here
-  if word.size < 4
+  if calls_long.size < 4
     puts true
   end
 end
