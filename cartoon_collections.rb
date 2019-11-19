34d6438a313efaaf3659c769ea263d853c603454
Abraham Moreno <adiaz2821@gmail.com>
2019-11-19 15:06:19 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -12,7 +12,7 @@ def summon_captain_planet(veggies)  # code an argument here
   end
 end
 
-def long_planeteer_calls(calls_long) # code an argument here
+def long_planeteer_calls(word) # code an argument here
   # Your code here
   long_planeteer_calls.include?(calls_long)
 
