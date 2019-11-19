19c179fa2e4ef1883bd87928465dd9fc26a10dbf
Abraham Moreno <adiaz2821@gmail.com>
2019-11-19 14:39:38 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -1,9 +1,15 @@
-def roll_call_dwarves# code an argument here
+def roll_call_dwarves(name) # code an argument here
+  name.each_with_index do |names, index|
+    puts "#{index + 1} #{names}"
+  end 
   # Your code here
 end
 
-def summon_captain_planet# code an argument here
+def summon_captain_planet(veggies)  # code an argument here
   # Your code here
+  veggies.collect do |elements|
+    elements.capitalize << "! "
+  end 
 end
 
 def long_planeteer_calls# code an argument here
