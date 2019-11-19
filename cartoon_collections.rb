86c1de30bf8bc7a076a2945c1f89cfe2b22d4ab0
Abraham Moreno <adiaz2821@gmail.com>
2019-11-19 14:41:09 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,7 +8,7 @@ end
 def summon_captain_planet(veggies)  # code an argument here
   # Your code here
   veggies.collect do |elements|
-    elements.capitalize << "! "
+    elements.capitalize << "!"
   end 
 end
 
